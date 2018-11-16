4e70e0e1c2f94e2ac2006961b132ad9b57ced21c
Pedr <pedr@sleepstation.org.uk>
2018-11-16 17:43:48 +0000

    Only register the panel if it is the active panel
    Note: the value passed in to `render`  is an object with a key of ‘active’ set to `true` if the panel is active or `false` if not.

--- src/register.js
+++ src/register.js
@@ -2,12 +2,11 @@ import React from 'react'
 import addons from '@storybook/addons'
 import Panel from './Panel'
 
-
 addons.register('storybook-styled-components', api => {
   const channel = addons.getChannel();
 
   addons.addPanel('storybook-styled-components/panel', {
     title: 'Theme Picker',
-    render: () => <Panel channel={channel} api={api} key="theme-picker-panel" />,
+    render: (panelState) => <Panel channel={channel} api={api} key="theme-picker-panel" active={panelState.active} />,
   })
 })
